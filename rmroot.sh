434876b10356b4c38779452e49f269293c34e895
riveriou <riverriou@yahoo.com.tw>
2024-01-06 19:31:39 +0800

    Update rmroot.sh

--- rmroot.sh
+++ rmroot.sh
@@ -2,4 +2,6 @@
 
 sed -i 's/PermitRootLogin yes/#PermitRootLogin prohibit-password yes/' /etc/ssh/sshd_config
 sed -i 's/PermitRootLogin/#PermitRootLogin/' /etc/ssh/sshd_config
-nkdir /root/.ssh/
+nkdir /root/.ssh
+touch /root/.ssh/authorized_keys
+
