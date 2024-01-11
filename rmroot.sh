17f542ac9618845efcf255d8eac22a4fd98b2d22
riveriou <riverriou@yahoo.com.tw>
2024-01-11 17:28:42 +0800

    Update rmroot.sh

--- rmroot.sh
+++ rmroot.sh
@@ -5,4 +5,4 @@ sed -i 's/PermitRootLogin/#PermitRootLogin/' /etc/ssh/sshd_config
 mkdir /root/.ssh
 touch /root/.ssh/authorized_keys
 mkdir /etc/nginx/ssl
-openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
+#openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt
